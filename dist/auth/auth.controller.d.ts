8d9f8df8c1195b65657cbf6ebc233de9668ac144
Oscar Bernal <oscarbernal93@gmail.com>
2023-03-23 07:59:50 -0500

    Remueve la carpeta dist de git

--- dist/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CreateUserDto, UserDto } from './dto/user.dto';
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    signup(body: CreateUserDto): Promise<UserDto>;
-}
